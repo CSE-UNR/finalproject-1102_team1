e7ab603a73a69d11731f870f528d0244b00e6f59
1102-Martinez-Ervin <ervinm@nevada.unr.edu>
2024-04-29 17:23:39 +0000

    test

--- test.c
+++ test.c
@@ -1,5 +1,37 @@
-//Alexus Rowe and Ervin Martinez
+//Alexus Rowe and Ervin Martinez//
 
-blah blah blah
 
-yes
+
+#include<stdio.h>
+
+int defaultMenu();
+
+int main(){
+	int choice;
+	
+	choice = defaultMenu();
+
+
+
+
+
+
+	return 0;
+}
+
+int defaultMenu(){
+	
+	int menuChoice;
+	
+	printf("********Photo Menu********\n|");
+	printf("1. Load New Image       |\n|");
+	printf("2. Display Current Image|\n|");
+	printf("3. Edit Current Image   |\n|");
+	printf("0. Exit                 |\n");
+	printf("**************************\n");
+	printf("What would you like?: ");
+	scanf("%d", &menuChoice);
+	
+	return menuChoice;
+}
+	
