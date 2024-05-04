f317424d76fc35587bd6931e2bf6e9611ef94ed8
1102-Martinez-Ervin <ervinm@nevada.unr.edu>
2024-05-04 00:37:59 +0000

    test.c

--- test.c
+++ test.c
@@ -7,9 +7,11 @@
 #define CURRENT_IMAGE "image.txt"
 #define MAX_ROWS 500
 #define MAX_COLS 500
+#define MAX_FILE_NAME 50
 
 int defaultMenu();
 void displayCurrentImage(int image[][MAX_COLS], FILE* initialImage, int realRows, int realCols);
+void newImage(int row, int cols, int image[][cols], FILE* newPhoto);
 
 int main(){
 
@@ -24,7 +26,7 @@ int main(){
 		switch(choice){
 		
 			case 1:
-			
+				newImage(rows, cols, image, currentImage_fp);
 			break;
 			
 			case 2:
@@ -68,27 +70,58 @@ int defaultMenu(){
 	return menuChoice;
 }
 
+void newImage(int row, int cols, int image[][cols], FILE* newPhoto){
+	
+	char file[MAX_FILE_NAME+1];
+	printf("Enter a file you would like to load: ");
+	scanf("%s", file);
+	
+	newPhoto = fopen(file, "r");
+	
+	if(newPhoto == NULL){
+		printf("Unable to open file. Non-existent.\n");
+	}
+	else{
+
+
+
+	}
+
+
+}
+	
+
 void displayCurrentImage(int image[][MAX_COLS], FILE* initialImage, int realRows, int realCols){
 	
 	
 	//work in progress 
 	char temp;
+	int i;
 	
 	initialImage = fopen(CURRENT_IMAGE, "r");
 	
 	if(initialImage == NULL){
-		printf("Unable to open file.\n");
+		printf("Unable to open file. Non-existent.\n");
 	}
 	else{
-		for(realRows = 0; realRows <= MAX_ROWS; realRows++){
-			for(realCols = 0; realCols <= MAX_COLS; realCols++){
-				while(fscanf(initialImage, "%c", &temp) == 1){
+		
+		
+		for(int i = 0; i <= realRows; realRows++){
+			for(int j = 0; j <= realCols; j++){
 					fscanf(initialImage, "%d", &image[realRows][realCols]);
 					fclose(initialImage);
-					printf("%d", image[realRows][realCols]);
-				
-				}
-			
+					if (image[realRows][realCols] = 0){
+						printf(" ");
+					}else if(image[realRows][realCols] = 1){
+						printf(".");
+					}else if(image[realRows][realCols] = 2){
+						printf("o");
+					}else if(image[realRows][realCols] = 3){
+						printf("O");
+					}else if(image[realRows][realCols] = 4){
+						printf("0");
+					}
+				//}
 			}
 		}
 		
