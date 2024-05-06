578592e4c893c459e5f2effe92b2896a6f72e85d
1102-Martinez-Ervin <ervinm@nevada.unr.edu>
2024-05-06 00:08:03 +0000

    test.c

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 
 
 #include<stdio.h>
+#include<stdbool.h>
 
 #define CURRENT_IMAGE "test_image.txt"
 #define MAX_ROWS 500
@@ -10,10 +11,12 @@
 #define MAX_FILE_NAME 50
 
 int defaultMenu();
-void displayCurrentImage(int image[][MAX_COLS], FILE* initialImage, int realRows, int realCols);
+void displayCurrentImage(int realRows, int realCols,int image[][realCols], FILE* initialImage);
 void newImage(int row, int cols, int image[][cols], FILE* newPhoto);
 void editImage(int image[][MAX_COLS], int rows, int columns);
 int rowSize(FILE* Image, int Rows, int Cols, int size[][Cols]);
+
+int sizeOfArray(FILE* currentFile, int row, int* cols, int enteredImage[][MAX_COLS]);
 
 int main(){
 
@@ -32,11 +35,13 @@ int main(){
 			break;
 			
 			case 2:
-				displayCurrentImage(image, currentImage_fp, rows, cols);
+				displayCurrentImage(rows, cols, image, currentImage_fp);
 			break;
 			
 			case 3:
-			
+				rows = sizeOfArray(currentImage_fp, rows, &cols, image);
+				printf("%d rows.\n", rows);
+				printf("%d cols.\n", cols);
 			break;
 			
 			case 0:
@@ -84,22 +89,64 @@ void newImage(int row, int cols, int image[][cols], FILE* newPhoto){
 		printf("Unable to open file. Non-existent.\n");
 	}
 	else{
-
-
+		//call for size of array to get size of row & cols
+		// hardcoded for now
+		
+		row = 12;
+		cols = 21;
+		
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < cols; j++){
+				fscanf(newPhoto, "%d", &image[i][j]);
+			}
+		}
+		fclose(newPhoto);
+		printf("\n");
+		printf("Successfully loaded new image!\n");
+		printf("\n");
 
 	}
-
-
+	
 }
 
+int sizeOfArray(FILE* currentFile, int row, int* cols, int enteredImage[][MAX_COLS]){
+	char temp[MAX_ROWS][MAX_COLS];
+	row = 0; 
+	int colsCount = 0;
+	currentFile = fopen(CURRENT_IMAGE, "r");
+	if(currentFile == NULL){
+		printf("Unable to open file. Non-existent.\n");
+	}
+	else{
+	
+
+	
+	
+		while(fscanf(currentFile, "%d", &enteredImage[row][MAX_ROWS]) == 1){
+			row++;
+		}
+		while(fscanf(currentFile, "%c", &temp[row][MAX_ROWS]) == 0){
+			colsCount++;
+	
+		
+		}
+		fclose(currentFile);
+		*cols = colsCount;
+	
+	return row;
+	}
+}
+	
 
 
-void displayCurrentImage(int image[][MAX_COLS], FILE* initialImage, int realRows, int realCols){
+
+
+void displayCurrentImage(int realRows, int realCols,int image[][realCols], FILE* initialImage){
 	
 	
 	//work in progress 
 	char temp;
-	
+	int type;
 
 	
 	initialImage = fopen(CURRENT_IMAGE, "r");
@@ -108,13 +155,22 @@ void displayCurrentImage(int image[][MAX_COLS], FILE* initialImage, int realRows
 		printf("Unable to open file. Non-existent.\n");
 	}
 	else{
-		realRows = 12;
-		realCols = 20;
-		while(fscanf(initialImage, "%c", &temp) == 1){
+	 	realRows = 12;
+		realCols = 21;
+	
+		//while(fscanf(initialImage, "%c", &temp) == 1){
+	
 			for(int i = 0; i < realRows; i++){
 				for(int j = 0; j < realCols; j++){
 					fscanf(initialImage, "%d", &image[i][j]);
-					
+				}
+			}	
+	
+		//}
+		fclose(initialImage);
+		for(int i = 0; i < realRows; i++){
+				for(int j = 0; j < realCols; j++){
+					//type = image[i][j];
 					if (image[i][j] = 0){
 						printf(" ");
 					}else if(image[i][j] = 1){
@@ -127,13 +183,11 @@ void displayCurrentImage(int image[][MAX_COLS], FILE* initialImage, int realRows
 						printf("0");
 					}
 				}
-				printf("\n");
-			}
+			printf("\n");
 		}
-		fclose(initialImage);
+	
 		
 	}
-
 }
 
 void editImage(int image[][MAX_COLS], int rows, int columns) {
